5d346d8838b0c3cd925ed1397a3d1ab56cf7d830
Sergey Lyubimov <see1rg@mail.ru>
2022-10-16 17:06:27 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    example();
+
+    }
+    static void example() {
+        List<List<String>> biDemArrList = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            biDemArrList.add(i, new ArrayList<>());
+                for (int j = 0; j < 8; j++) {
+                    if ((j+i) % 2 == 0) {
+                        biDemArrList.get(i).add(j, "◯");
+                    } else {
+                        biDemArrList.get(i).add(j, "●");
+                    }
+                }
+            }
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(biDemArrList.get(i).get(j) + " ");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
